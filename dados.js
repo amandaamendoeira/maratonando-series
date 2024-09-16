26aa74c40e2281e57ee1c0c4ae2574329971ba93
Amanda Amendoeira <145858289+amandaamendoeira@users.noreply.github.com>
2024-09-16 19:13:17 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+  {
+    titulo: "Doctor Who",
+    descricao:
+      "Doctor Who é uma série de ficção científica britânica que acompanha as aventuras do Doutor, um alienígena viajante do tempo. A bordo da TARDIS, uma máquina do tempo disfarçada de cabine telefônica, ele explora o universo, enfrenta criaturas e ajuda pessoas em diversas épocas. A cada nova regeneração, o Doutor assume uma nova aparência, garantindo aventuras sempre renovadas.",
+    link: "https://tardis.fandom.com/wiki/Doctor_Who_Wiki",
+  },
+  {
+    titulo: "Prison Break",
+    descricao:
+      "Prison Break é uma série dramática que acompanha a história de Michael Scofield, um engenheiro que planeja um elaborado plano de fuga para tirar seu irmão, Lincoln Burrows, de uma prisão de segurança máxima, onde foi condenado injustamente por um crime que não cometeu.",
+    link: "https://pt.wikipedia.org/wiki/Prison_Break",
+  },
+  {
+    titulo: "Outlander",
+    descricao:
+      "Outlander é uma série de drama histórico e romance que acompanha a história de Claire Randall, uma enfermeira da Segunda Guerra Mundial que, durante uma viagem à Escócia, é misteriosamente transportada para a Escócia do século XVIII, onde se envolve em uma apaixonante e perigosa história de amor com um guerreiro escocês.",
+    link: "https://pt.wikipedia.org/wiki/Outlander_(série_de_televisão)",
+  },
+  {
+    titulo: "House M.D.",
+    descricao:
+      "House M.D. é uma série médica que acompanha o Dr. Gregory House, um médico genial e sarcástico, e sua equipe em um hospital universitário. House é conhecido por sua capacidade de diagnosticar doenças complexas e raras, mesmo quando outros médicos falham. Com métodos pouco convencionais e uma personalidade controversa, ele lidera sua equipe em busca de diagnósticos precisos, desafiando as regras e os limites da medicina tradicional.",
+    link: "https://house.fandom.com/wiki/House_Wiki",
+  },
+];
